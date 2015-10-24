15a393bb1a0b7b8ee44933d19e14b43b8a676976
Felipe Baccin <felipebaccincvel@gmail.com>
2015-10-24 11:24:17 -0200

    primeiro commit do projeto jasper

--- componentes/README.md
+++ componentes/README.md
@@ -1,1 +1,16 @@
-# reoferta20151poo
+# reoferta2015lpoo
+
+
+# Instruções iniciais do dia 24 de outubro de 2015
+
+* Criar repositório no Github chamado reoferta2015lpoo.
+
+* Marcar "Initialize this repository with a README" no momento da criação.
+
+* Clonar o repositório.
+
+* Criar projeto Maven chamado componentes dentro do repositório.
+
+
+Para depois do intervalo: Copiar arquivo TIBCOJaspersoftStudio-6.1.1.final-windows-x86.zip
+da pasta \\server1\docentes\TADS\FernandoDagostini\2015\Instalações
--- componentes/src/main/java/br/univel/custompanel/Fundo.java
+++ componentes/src/main/java/br/univel/custompanel/Fundo.java
@@ -8,9 +8,11 @@ import javax.swing.JPanel;
 
 public class Fundo extends JPanel {
 
+	private static Color corCasa = Color.BLACK;
+	
 	public Fundo() {
 		setBackground(Color.WHITE);
-		
+				
 	}
 
 	@Override
@@ -18,10 +20,21 @@ public class Fundo extends JPanel {
 		super.paintComponent(g);
 		
 		Graphics2D g2 = (Graphics2D) g;
-		g2.setColor(Color.BLACK);
-		g2.fillRect(0, 0, getWidth()-1, 30);
-		g2.setColor(Color.YELLOW);
-		g2.fillRect(60, 60,60, getHeight()-1 );
+		
+		int largura = getWidth()/8;
+		int autura = getHeight()/8;
+		
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				g2.setColor(corCasa);
+			//	g2.fillRect(i*50,j *50,larguraQuad,auturaQuad);
+			}
+		}
+		
+//		g2.setColor(Color.BLACK);
+//		g2.fillRect(0, 0, getWidth()-1, 30);
+//		g2.setColor(Color.YELLOW);
+//		g2.fillRect(60, 60,60, getHeight()-1 );
 	}
 
 }
